552db5bf90b77a4bdf6f478c53adb97aaa52b037
samichafik <samichafik00@gmail.com>
2023-12-07 16:33:11 -0800

    tasks

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,11 @@
 nclude "lists.h"
 
 /**
- *  * dlistint_len - count the number of nodes in the linked list
- *   * @h: pointer to the beginning of a linked list
- *    *
- *     * Return: number of nodes
- *      */
+ * dlistint_len - count the number of nodes in the linked list
+ * @h: pointer to the beginning of a linked list
+ *
+ * Return: number of nodes
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i;
