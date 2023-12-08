0b41ba0d8adfeba4e357e48b1e36628bf69fc21a
samichafik <samichafik00@gmail.com>
2023-12-07 17:11:13 -0800

    tasks

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * * free_dlistint - free a dlistint_t list
- * * @head: pointer to the beginning of the linked list
- * *
- * * Return: void
- * */
+ * free_dlistint - free a dlistint_t list
+ * @head: pointer to the beginning of the linked list
+ *
+ * Return: void
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *next;
